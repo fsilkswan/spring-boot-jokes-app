3151f60b55061ca081f6cbdd8ab61e7d593bbe3d
fseidenschwan <ferdinand@seidenschwan.de>
2018-11-14 22:36:23 +0100

    Code formatting.

--- src/main/java/guru/springframework/SpringBootJokesAppApplication.java
+++ src/main/java/guru/springframework/SpringBootJokesAppApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJokesAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJokesAppApplication.class, args);
-	}
+public class SpringBootJokesAppApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootJokesAppApplication.class, args);
+    }
 }
